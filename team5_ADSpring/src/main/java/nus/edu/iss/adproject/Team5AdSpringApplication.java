f6368d25b6574ba1291f07be8ea1bd76cc69bcf1
jingxuan94 <E0639666@u.nus.edu>
2021-01-29 14:36:30 +0800

    project setup

---
+++ team5_ADSpring/src/main/java/nus/edu/iss/adproject/Team5AdSpringApplication.java
@@ -0,0 +1,13 @@
+package nus.edu.iss.adproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5AdSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team5AdSpringApplication.class, args);
+	}
+
+}
---
+++ team5_ADSpring/src/test/java/nus/edu/iss/adproject/Team5AdSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.edu.iss.adproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team5AdSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
