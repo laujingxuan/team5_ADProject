f2104c8718728814f7f50fbab8f96fba859f74e9
tanfeng95 <31460702+tanfeng95@users.noreply.github.com>
2021-01-29 15:15:56 +0800

    booking, booking detail and product review entity

---
+++ team5_ADSpring/src/main/java/nus/edi/iss/adproject/repository/BookingDetailsRepo.java
@@ -0,0 +1,9 @@
+package nus.edi.iss.adproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import nus.edi.iss.adproject.model.BookingDetails;
+
+public interface BookingDetailsRepo  extends JpaRepository<BookingDetails, Long>{
+
+}
---
+++ team5_ADSpring/src/main/java/nus/edi/iss/adproject/repository/ProductReviewRepo.java
@@ -0,0 +1,9 @@
+package nus.edi.iss.adproject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import nus.edi.iss.adproject.model.ProductReview;
+
+public interface ProductReviewRepo extends JpaRepository<ProductReview, Long> {
+
+}
