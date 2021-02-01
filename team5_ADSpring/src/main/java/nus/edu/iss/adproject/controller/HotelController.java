cba742a7b72647224a20c98b8ab6f725cefc9453
LuSicong <69235232+LuSicong22@users.noreply.github.com>
2021-02-01 21:05:47 +0800

    Added edit and save action to Hotel Controller Added productForm.html

--- team5_ADSpring/src/main/java/nus/edu/iss/adproject/controller/HotelController.java
+++ team5_ADSpring/src/main/java/nus/edu/iss/adproject/controller/HotelController.java
@@ -4,16 +4,20 @@ package nus.edu.iss.adproject.controller;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import nus.edu.iss.adproject.model.Hotel;
 import nus.edu.iss.adproject.service.HotelService;
+
 
 @Controller
 @RequestMapping("/hotel")
@@ -38,6 +42,23 @@ public class HotelController {
 		model.addAttribute("hotels",hotelservice.findById(id));
 		return "Map";
 	}
+	
+	@GetMapping("/edit/{id}")
+	public String showEditForm(Model model, @PathVariable("id") Long id) {
+		model.addAttribute("hotel", hotelservice.findById(id));
+		return "productForm";
+	}
+	
+	@GetMapping("/save")
+	public String saveProductForm(@ModelAttribute("product") @Valid Hotel hotel, BindingResult bindingResult,
+			Model model) {
+		if (bindingResult.hasErrors()) {
+			return "productform";
+		}
+		hotelservice.save(hotel);
+		return "forward:/hotel/Hotel";
+	}
+	
 
 
 }
--- team5_ADSpring/src/test/java/nus/edu/iss/adproject/HotetTEST.java
+++ team5_ADSpring/src/test/java/nus/edu/iss/adproject/HotetTEST.java
@@ -11,26 +11,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import nus.edu.iss.adproject.model.Hotel;
 import nus.edu.iss.adproject.repository.HotelRepository;
 
-//@SpringBootTest
-//public class HotetTEST {
-//
-//	@Autowired
-//	private HotelRepository htRepo;
-//	@Test
-//	void htcreate()
-//	{
+@SpringBootTest
+public class HotetTEST {
+
+	@Autowired
+	private HotelRepository htRepo;
+	@Test
+	void htcreate()
+	{
+		
+		htRepo.save(new Hotel(null, "Marina Bay Sands Hotel ", null, 1.282302,103.858528 , 400.0, 8, "SG","aa", "best", "we try to be home", null));
+		htRepo.save(new Hotel(null, "Swissôtel The Stamford ", null, 1.293354,103.853561 , 260.0, 8, "SG","aa", "best", "we try to be home", null));
+		htRepo.save(new Hotel(null, "Hotel Miramar ", null, 1.288710,103.837372 , 260.0, 8, "SG","aa", "best", "we try to be home", null));
+//		ArrayList<Hotel> plist = new ArrayList<Hotel>();		
+//		plist = (ArrayList<Hotel>) htRepo.findAll();
 //		
-//		htRepo.save(new Hotel("Marina Bay Sands Hotel ", 1.282302,103.858528 , 400.0, 8, "SG","aa", "best", "we try to be home"));
-//		htRepo.save(new Hotel("Swissôtel The Stamford ", 1.293354,103.853561 , 260.0, 8, "SG","aa", "best", "we try to be home"));
-//		htRepo.save(new Hotel("Hotel Miramar ", 1.288710,103.837372 , 260.0, 8, "SG","aa", "best", "we try to be home"));
-////		ArrayList<Hotel> plist = new ArrayList<Hotel>();		
-////		plist = (ArrayList<Hotel>) htRepo.findAll();
-////		
-////		System.out.println("Print all products");
-////		for (Iterator<Hotel> iterator = plist.iterator(); iterator.hasNext();) {
-////			Hotel product =  iterator.next();
-////			System.out.println(product.toString());
-////		}
-//	}
-//	
-//}
+//		System.out.println("Print all products");
+//		for (Iterator<Hotel> iterator = plist.iterator(); iterator.hasNext();) {
+//			Hotel product =  iterator.next();
+//			System.out.println(product.toString());
+//		}
+	}
+	
+}
