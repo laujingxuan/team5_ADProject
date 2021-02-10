f7d83f01a260dfad7e5bfacb9077554c28669fe6
TianMyy <mingyu_tian@outlook.com>
2021-02-10 21:04:40 +0800

    save as blob

--- team5_ADSpring/src/main/java/nus/edu/iss/adproject/controller/ReviewController.java
+++ team5_ADSpring/src/main/java/nus/edu/iss/adproject/controller/ReviewController.java
@@ -1,11 +1,19 @@
 package nus.edu.iss.adproject.controller;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,8 +30,8 @@ import org.springframework.web.multipart.MultipartFile;
 import nus.edu.iss.adproject.model.Product;
 import nus.edu.iss.adproject.model.ProductReview;
 import nus.edu.iss.adproject.model.User;
-import nus.edu.iss.adproject.nonEntityModel.FileUploadUtil;
 import nus.edu.iss.adproject.repository.ProductRepo;
+import nus.edu.iss.adproject.repository.ProductReviewRepo;
 import nus.edu.iss.adproject.service.ProductReviewService;
 import nus.edu.iss.adproject.service.SessionService;
 
@@ -35,6 +43,9 @@ public class ReviewController {
 	ProductReviewService prservice;
 	
 	@Autowired
+	ProductReviewRepo prrepo;
+	
+	@Autowired
 	ProductRepo prepo;
 	
 	@Autowired
@@ -42,15 +53,33 @@ public class ReviewController {
 	
 	
 	@GetMapping("/list/{id}")
-	public String listProductReview(Model model, @PathVariable("id")Long id) {
+	public String listProductReview(Model model, @PathVariable("id")Long id) throws IOException {
 		Product product = prepo.findById(id).get();
 		List<ProductReview> reviewList = prservice.findReviewByProductId(id);
+		List<BufferedImage> image1 = new ArrayList<>();
+		for (ProductReview p :reviewList) {
+			byte[] picture = p.getPic();
+			InputStream is = new ByteArrayInputStream(picture);
+		    BufferedImage bImage2 = ImageIO.read(is);
+		    image1.add(bImage2);
+		}
+		
 		model.addAttribute("review", reviewList); 
 		model.addAttribute("product", product);
-		
+		model.addAttribute("photo", image1);
 		return "reviewList";
 	}
 	
+	@GetMapping("/product/image/{id}")
+	public void showProductImage(@PathVariable Long id,
+	                               HttpServletResponse response) throws IOException {
+	response.setContentType("image/jpeg"); // Or whatever format you wanna use
+
+	ProductReview review = prrepo.findById(id);
+
+	InputStream is = new ByteArrayInputStream(product.getImage());
+	IOUtils.copy(is, response.getOutputStream());
+	}
 	
 	@GetMapping("/post/{id}")
 	public String postProductReview(Model model, HttpSession session, @PathVariable("id")Long id) {
@@ -70,18 +99,24 @@ public class ReviewController {
 			@ModelAttribute("review")@Valid ProductReview review, 
 			BindingResult bindingResult, @RequestParam("image") MultipartFile multipartFile) throws IOException {
 		String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+		System.out.println(fileName);
+		File convFile = new File(System.getProperty("java.io.tmpdir") + "/" + fileName);
+		multipartFile.transferTo(convFile);
+		byte[] fileContent = FileUtils.readFileToByteArray(convFile);
+		review.setPic(fileContent);
 		review.setPhoto(fileName);
-		String photoImagePath = "/user-photos/" + review.getUser().getId() + "/" + review.getPhoto();
-		review.setPhotoImagePath(photoImagePath);
+//		String photoImagePath = "/user-photos/" + review.getUser().getId() + "/" + review.getPhoto();
+//		review.setPhotoImagePath(photoImagePath);
 		review.setProduct(prepo.findById(id).get());
 		if(bindingResult.hasErrors()) {
 			return "reviewForm";
 		}
 		prservice.save(review);
-		String uploadDir = "user-photos/" + review.getUser().getId();
-		FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+//		String uploadDir = "user-photos/" + review.getUser().getId();
+//		FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
 		return "reviewList";
 	}
+	
 	
 	
 }
--- team5_ADSpring/src/main/java/nus/edu/iss/adproject/nonEntityModel/MvcConfig.java
+++ team5_ADSpring/src/main/java/nus/edu/iss/adproject/nonEntityModel/MvcConfig.java
@@ -10,18 +10,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
  
- 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        exposeDirectory("user-photos", registry);
-    }
-     
-    private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
-        Path uploadDir = Paths.get(dirName);
-        String uploadPath = uploadDir.toFile().getAbsolutePath();
-         
-        if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
-         
-        registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/"+ uploadPath + "/");
-    }
+// 
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        exposeDirectory("user-photos", registry);
+//    }
+//     
+//    private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
+//        Path uploadDir = Paths.get(dirName);
+//        String uploadPath = uploadDir.toFile().getAbsolutePath();
+//         
+//        if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
+//         
+//        registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/"+ uploadPath + "/");
+//    }
 }
