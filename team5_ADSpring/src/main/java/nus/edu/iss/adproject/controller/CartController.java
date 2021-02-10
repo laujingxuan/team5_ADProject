97bf801bf813f48d3419c30c97ce928cd89cdd1a
jingxuan94 <E0639666@u.nus.edu>
2021-02-10 18:01:08 +0800

    add, edit, prices cart done

--- team5_ADSpring/src/main/java/nus/edu/iss/adproject/controller/CartController.java
+++ team5_ADSpring/src/main/java/nus/edu/iss/adproject/controller/CartController.java
@@ -1,7 +1,5 @@
 package nus.edu.iss.adproject.controller;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +9,8 @@ import javax.validation.Valid;
 
 //import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,19 +18,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
-import net.minidev.json.JSONObject;
+import nus.edu.iss.adproject.model.Attraction;
 import nus.edu.iss.adproject.model.Cart;
+import nus.edu.iss.adproject.model.Hotel;
+import nus.edu.iss.adproject.model.TravelPackage;
 import nus.edu.iss.adproject.model.User;
 import nus.edu.iss.adproject.nonEntityModel.CartForm;
+import nus.edu.iss.adproject.nonEntityModel.CartWrapper;
+import nus.edu.iss.adproject.nonEntityModel.DailyRoomDetailWrapper;
+import nus.edu.iss.adproject.nonEntityModel.DailyRoomTypeDetail;
+import nus.edu.iss.adproject.nonEntityModel.MultipleDateQuery;
+import nus.edu.iss.adproject.nonEntityModel.ProductType;
 import nus.edu.iss.adproject.service.CartService;
-import nus.edu.iss.adproject.service.CartServiceImpl;
 import nus.edu.iss.adproject.service.ProductService;
 import nus.edu.iss.adproject.service.SessionService;
-import nus.edu.iss.adproject.service.SessionServiceImpl;
+import nus.edu.iss.adproject.service.TravelPackageService;
 
 
 @Controller
@@ -47,69 +54,35 @@ public class CartController {
 	private ProductService pservice;
 	
 	@Autowired
-	public void SetImplimentation(CartServiceImpl cart_svcimpl, SessionServiceImpl session_svcimpl) {
-		this.cart_svc = cart_svcimpl;
-		this.session_svc = session_svcimpl;
-	}
+	private TravelPackageService tpservice;
 
-//	@InitBinder()
-//	protected void initBinder(WebDataBinder binder) {
-//		
-//	}
-
-	@ResponseBody
-	@GetMapping("/getQuantityByUserId")
-	public JSONObject CartitemTotalQuantiy( Model model, HttpSession session) {
-		 //String s = "{\"status\":\"success\", \"total\": 5}";
-		
-		  if (session_svc.isNotLoggedIn(session)) return null; 
-		  User user = (User)session.getAttribute("user"); 
-		  Long userId = user.getId(); 
-		  int quantity =  cart_svc.getQuantityByUserId(userId); 
-		  //System.out.println(quantity);
-		  
-		  //Map<String, Object> map=new HashMap<String,Object>(); 
-		  //map.put("quantity",quantity);
-		  
-		  Map<String, Object> map=new HashMap<String,Object>(); 
-		  map.put("status", "success");
-		  map.put("total", quantity);
-		  
-		  JSONObject jsonObj=new JSONObject(map);
-		  System.out.println("checkpoint A ");
-		  System.out.println("checkpoint B: " + jsonObj.toJSONString());
-		
-		//return jsonObj;
-		return jsonObj;
-	}
-	
-	@ResponseBody
-	@RequestMapping("/getProductQuantity")
-	public JSONObject getProductQuantity(CartForm cartitem ,Model model, HttpSession session) {
-		  if (session_svc.isNotLoggedIn(session)) return null; User user = (User)
-		  session.getAttribute("user"); Long userId = user.getId(); 
-		  //int quantity =cart_svc.getQuantityByUserId(userId);
-		  //System.out.println(quantity);
-		  String startdate = cartitem.getStartDate();
-		  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("mm/dd/yy");
-		  LocalDate startDate = LocalDate.parse(startdate, formatter);
-		  
-		  int quantity =cart_svc.getProductQuantity(cartitem.getProductId(),cartitem.getUserId(),startDate);
-		  System.out.print("now quantity is " + quantity);
-		  
-		  //Map<String, Object> map=new HashMap<String,Object>(); 
-		  //map.put("quantity",quantity);		  
-		  Map<String, Object> map=new HashMap<String,Object>(); 
-		  map.put("status", "success");
-		  map.put("total", quantity);
-		  
-		  JSONObject jsonObj=new JSONObject(map);
-		  System.out.println("checkpoint C: " + jsonObj.toJSONString());
-		
-		//return jsonObj;
-		return jsonObj;
-	}
-	
+	//done
+	//view cart
+    @GetMapping("/list")
+    public String ListCartItems(Model model, HttpSession session){    
+    	if (session_svc.isNotLoggedIn(session)) return "redirect:/user/login";
+    	User user = (User) session.getAttribute("user");
+        List<Cart> carts = cart_svc.findByUserId(user.getId());
+        Map<Cart, Double> cartMap = new HashMap<Cart,Double>();
+        //price for 1 room/1 attraction
+        double price;
+        for (Cart cart: carts) {
+        	if(cart.getProduct().getType() == ProductType.HOTEL) {
+        		price = getTotalNightPriceAfterHotelDiscount(cart);
+        	}else {
+        		price = getTotalPriceAfterAttractionDiscount(cart);
+        	}
+        	cartMap.put(cart, price);
+        }
+        
+		List<TravelPackage> tpList = tpservice.retrieveAll();
+		model.addAttribute("tpList", tpList);
+        model.addAttribute("carts", cartMap);
+        return "cart";
+    }
+    
+	//done
+	//add to cart
 	@PostMapping("/add/{id}")
 	public String AddItemToCart(@ModelAttribute("cartitem") @Valid CartForm cartitem, @PathVariable("id") Long productId, BindingResult bindingResult, Model model, HttpSession session) {
 		if (session_svc.isNotLoggedIn(session)) return "redirect:/user/login";
@@ -123,79 +96,63 @@ public class CartController {
 		cart_svc.save(newCart);
 		return "redirect:/cart/list";
 	}
-
-    @GetMapping("/list")
-    public String ListCartItems(Model model, HttpSession session){    
-    	if (session_svc.isNotLoggedIn(session)) return "redirect:/user/login";
-    	User user = (User) session.getAttribute("user");
-        List<Cart> carts = cart_svc.findByUserId(user.getId());  
-        
-        System.out.println(carts.size());
-        
-        for (Cart c : carts) {
-        	System.out.println(c);
-        	System.out.println(c.toString());
-        }
-        	
-        model.addAttribute("carts", carts);
-        return "Cart_chloe";
-    }
-    
+	
+	//done
+	@PostMapping("/update")
+	@ResponseBody
+	public ResponseEntity<Boolean> editCart(@RequestBody CartWrapper wrapper, HttpSession session) {
+		Cart cart = cart_svc.findById(Long.parseLong(wrapper.getCartId()));
+		cart.setQuantity(Integer.parseInt(wrapper.getQuantity()));
+		cart.setNumGuests(Integer.parseInt(wrapper.getNumGuest()));
+		if (!wrapper.getRemarks().equals("attractionPlaceHolder")) {
+			cart.setRemarks(wrapper.getRemarks());
+		}
+		cart_svc.save(cart);
+		return new ResponseEntity<Boolean>(true, HttpStatus.OK);
+	}
+	
 	@GetMapping("/delete/{id}")
 	public String deleteMethod(Model model, @PathVariable("id") Long id) {
 		Cart cartitem = cart_svc.findById(id);
 		cart_svc.delete(cartitem);
 		return "forward:/cart/list";
 	}
+	
+	public double getTotalNightPriceAfterHotelDiscount(Cart cart) {
+		final String uri = cart.getProduct().getRoomType().getHotel().getAPI_URL()+ "room/period";	
+	    RestTemplate restTemplate = new RestTemplate();
+	    MultipleDateQuery query = new MultipleDateQuery(cart.getStartDate(), cart.getEndDate(), cart.getProduct().getRoomType().getRoomType());
+	    DailyRoomDetailWrapper result = restTemplate.postForObject( uri, query, DailyRoomDetailWrapper.class);
+	    double total = 0;
+    	for (DailyRoomTypeDetail daily: result.getDailyList()) {
+    		Hotel hotel = cart.getProduct().getRoomType().getHotel();
+    		double discount_rate = 0;
+    		//getting the largest discount rate for that day (The special discount set by the hotel manager)
+    		for(int i = 0; i < hotel.getDiscount().size(); i++) {
+    			if(daily.getDate().isAfter(hotel.getDiscount().get(i).getFrom_date()) && daily.getDate().isBefore(hotel.getDiscount().get(i).getTo_date())){
+    				if(hotel.getDiscount().get(i).getDiscount_rate()>discount_rate) {
+    					discount_rate = hotel.getDiscount().get(i).getDiscount_rate();
+    				}
+    			}
+    		}
+	    	total += daily.getDailyPrice()*(100-discount_rate)/100;
+	    }
+    	return total;
+	}
+	
+	public double getTotalPriceAfterAttractionDiscount(Cart cart) {	
+		//calculate the price based on the original price and discount percentage;
+		Attraction attraction = cart.getProduct().getAttraction();
+		double discount_rate = 0;
+		//check every discount under the attraction and get the largest applicable discount
+		for (int i = 0; i < attraction.getDiscount().size(); i++) {
+			if (cart.getStartDate().isAfter(attraction.getDiscount().get(i).getFrom_date()) && cart.getStartDate().isBefore(attraction.getDiscount().get(i).getTo_date())) {
+				if (attraction.getDiscount().get(i).getDiscount_rate()>discount_rate) {
+					discount_rate = attraction.getDiscount().get(i).getDiscount_rate();
+				}
+			}
+		}
+		return cart.getProduct().getAttraction().getPrice() * (100-discount_rate)/100;
+	}
+	
 }
-    //receive JSON data from Add.js. (When an item is added to the cart from gallery)
-	
-	/*
-	@RequestMapping("/add")
-    public JSONObject AddItemToCart([FromBody] Addinput product) {
-        
-        int total = cart_svc.add(product);
-        
-        return new JSONObject("{'status':'success', 'total': total}");
-    }*/
-
-    /*
-    @PostMapping("/save")
-    public void ChangeCartItemQuantity([FromBody] ChangeInput change) { //receive JSON object from Cart.js when the number in the cart is changed
-        
-        Cart item = cart_svc.findByUserIdAndProductId(userId, change.ProductId);
-        item.setQuantity(new_qty);
-        cart_svc.save(item);
-        
-        return null;
-    }*/
-
-    /*
-    [HttpPost]
-    public JSONObject DeleteCartItem([FromBody] Addinput item) {
-        
-        Cart item = cart_svc.findByUserIdAndProductId(userId, change.ProductId);
-        cart_svc.delete(item);
-
-        return new JSONObject("{'status':'success'}");
-        }
-        */
-   
-/*
-@GetMapping("/delete/{id}")
-public String deleteMethod1(Model model, @PathVariable("id") Long id) {
-	Cart cartitem = cart_svc.findById(id);
-	cart_svc.delete(cartitem);
-	return "forward:/cart/list";
-}*/
-
-    /*
-    [HttpPost] 
-    public String Checkout() { // checkout is form deletion from Cart and adding to PurchaseHistory
-
-        List<Cart> carts = cart_svc.findByUserId(userId);
-        cart_svc.checkout(carts);
-
-        return "/";
-    }*/
-	
