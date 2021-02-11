4b7b0ef4621f15d2710c0fc48cdca48d73102499
Eaint Chit Thae <e0641604@u.nus.edu>
2021-02-11 09:54:55 +0800

    bug fixing for discount

--- team5_ADSpring/src/main/java/nus/edu/iss/adproject/controller/DiscountController.java
+++ team5_ADSpring/src/main/java/nus/edu/iss/adproject/controller/DiscountController.java
@@ -3,16 +3,22 @@ package nus.edu.iss.adproject.controller;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -20,11 +26,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 import nus.edu.iss.adproject.model.Attraction;
 import nus.edu.iss.adproject.model.Discount;
 import nus.edu.iss.adproject.model.Hotel;
+import nus.edu.iss.adproject.model.Month;
 import nus.edu.iss.adproject.model.User;
+import nus.edu.iss.adproject.nonEntityModel.DailyRoomDetailWrapper;
+import nus.edu.iss.adproject.nonEntityModel.DailyRoomTypeDetail;
+import nus.edu.iss.adproject.nonEntityModel.MonthTypeQuery;
+import nus.edu.iss.adproject.nonEntityModel.RoleType;
 import nus.edu.iss.adproject.service.AttractionService;
 import nus.edu.iss.adproject.service.AttractionServiceImpl;
 import nus.edu.iss.adproject.service.DiscountService;
@@ -58,6 +72,8 @@ public class DiscountController {
 		this.session_svc = session_svcimpl;
 	}
 	
+	Discount discountData = new Discount();
+	
 	@InitBinder()
 	protected void initBinder(WebDataBinder binder) {
 		
@@ -66,6 +82,37 @@ public class DiscountController {
 	@GetMapping("/save")
 	public String saveDiscountForm(@ModelAttribute("discount") @Valid Discount discount, BindingResult bindingResult,
 			Model model,HttpSession session)  {
+		User user = (User) session.getAttribute("user");
+		
+		if (session_svc.isNotLoggedIn(session))
+			return "redirect:/user/login";
+		List<Hotel> hotel = hotel_svc.findByUserId(user.getId());
+		List<Attraction> attraction = attraction_svc.findByUserId(user.getId());
+		if(hotel.size()> 0) {
+			model.addAttribute("name", "Hotel");
+			model.addAttribute("hotel", hotel);
+		} else if(attraction.size()>0) {
+			model.addAttribute("name", "Attraction");
+			model.addAttribute("attraction", attraction);
+		}
+		if(discount.getFrom_date() == null) {
+			bindingResult.addError(new FieldError("discountForm","from_date","From date shoule be empty"));
+			return "discountForm";
+		}
+		else if(discount.getTo_date() == null) {
+			bindingResult.addError(new FieldError("discountForm","to_date","To date shoule be empty"));
+			return "discountForm";
+		}
+		else if(discount.getTo_date().isBefore(discount.getFrom_date())) {
+			bindingResult.addError(new FieldError("discountForm","from_date","From date shoule be less than To date"));
+			bindingResult.addError(new FieldError("discountForm","to_date","To date shoule be greater than from date"));
+			return "discountForm";
+		}
+		
+		else if (bindingResult.hasErrors()) {
+				
+			return "discountForm";
+		}
 		discount_svc.save(discount);
 		
 		/*
@@ -86,18 +133,22 @@ public class DiscountController {
 		User user = (User) session.getAttribute("user");
 		if (session_svc.isNotLoggedIn(session))
 			return "redirect:/user/login";
-		
-		List<Hotel> hotel = hotel_svc.findByUserId(user.getId());
-		List<Attraction> attraction = attraction_svc.findByUserId(user.getId());
-		if(hotel.size()> 0) {
-			model.addAttribute("name", "Hotel");
-			model.addAttribute("hotel", hotel);
-		} else if(attraction.size()>0) {
-			model.addAttribute("name", "Attraction");
-			model.addAttribute("attraction", attraction);
-		}		
-		model.addAttribute("discount", new Discount());
-		return "discountForm";
+		if(user.getRole() == RoleType.HOTELMANAGER || user.getRole() == RoleType.ATTRACTIONMANAGER) {
+			List<Hotel> hotel = hotel_svc.findByUserId(user.getId());
+			List<Attraction> attraction = attraction_svc.findByUserId(user.getId());
+			if(hotel.size()> 0) {
+				model.addAttribute("name", "Hotel");
+				model.addAttribute("hotel", hotel);
+			} else if(attraction.size()>0) {
+				model.addAttribute("name", "Attraction");
+				model.addAttribute("attraction", attraction);
+			}		
+			model.addAttribute("discount", new Discount());
+			return "discountForm";
+		}
+		else {
+			return "null";
+		}
 	}
 	
 	@GetMapping("/edit/{id}")
@@ -117,14 +168,21 @@ public class DiscountController {
 	@GetMapping("/list")
 	public String listDiscountForm(Model model,HttpSession session) {	
 		User user = (User) session.getAttribute("user");
-		List<Discount> hotel_dis = discount_svc.findDiscountByHotelUserId(user.getId());
-		List<Discount> attraction_dis = discount_svc.findDiscountByAttractionUserId(user.getId());
-		if(hotel_dis.size()>0) {
-			model.addAttribute("discounts", hotel_dis);
-		} else if(attraction_dis.size()>0) {
-			model.addAttribute("discounts", attraction_dis);
+		if (session_svc.isNotLoggedIn(session))
+			return "redirect:/user/login";
+		if(user.getRole() == RoleType.HOTELMANAGER || user.getRole() == RoleType.ATTRACTIONMANAGER) {
+			List<Discount> hotel_dis = discount_svc.findDiscountByHotelUserId(user.getId());
+			List<Discount> attraction_dis = discount_svc.findDiscountByAttractionUserId(user.getId());
+			if(hotel_dis.size()>0) {
+				model.addAttribute("discounts", hotel_dis);
+			} else if(attraction_dis.size()>0) {
+				model.addAttribute("discounts", attraction_dis);
+			}
+			return "discounts";
 		}
-		return "discounts";
+		else {
+			return null;
+		}
 	}
 	
 	@RequestMapping(value = "/delete/{id}")
